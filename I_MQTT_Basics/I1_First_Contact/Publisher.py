85ebdada4e46c85f82c955e78b358c044fd687c6
LivioDad <livio.dad@gmail.com>
2025-11-05 13:43:49 +0100

    I1 First Contact

---
+++ I_MQTT_Basics/I1_First_Contact/Publisher.py
@@ -0,0 +1,22 @@
+from MyMQTT import *
+import time
+
+BROKER = "broker.hivemq.com"
+PORT   = 1883
+TOPIC  = "hello"
+         #(self, clientID, broker, port, notifier):
+client = MyMQTT("publisher", BROKER, PORT, notifier=None)
+client.start()
+msg = "Hi from the MQTT publisher!"
+
+try:
+    while True:
+        time.sleep(2)
+        client.myPublish(TOPIC, msg)
+        print(f'Message "{msg}" published on topic {TOPIC}')
+except KeyboardInterrupt:
+    client.stop()
+    print("Publisher stopped succesfully")
+    pass
+finally:
+    client.stop()
---
+++ I_MQTT_Basics/I1_First_Contact/Subscriber.py
@@ -0,0 +1,27 @@
+from MyMQTT import *
+import time, json
+
+class Printer:
+    def notify(self, topic, payload):
+        try:
+            data = json.loads(payload)
+        except Exception:
+            data = payload.decode() if isinstance(payload, (bytes, bytearray)) else str(payload)
+        print(f'Recieved message on topic "{topic}": {data}')
+
+BROKER = "broker.hivemq.com"
+PORT   = 1883
+TOPIC  = "hello"
+
+client = MyMQTT("subscriber", BROKER, PORT, notifier=Printer())
+client.start()
+client.mySubscribe(TOPIC)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Subscriber stopped succesfully")
+    pass
+finally:
+    client.stop()
